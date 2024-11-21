755ab53f55cd11c524cd954842d04221be9d57c6
kuloth03 <kulothkuloth4361@gmail.com>
2024-11-21 11:29:23 +0530

    firstcommit

---
+++ src/main/java/com/automation/AutomationController.java
@@ -0,0 +1,13 @@
+package com.automation;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AutomationController {
+	@GetMapping(value = "/getValue")
+	public String get() {
+		return "Hello World";
+	}
+
+}
